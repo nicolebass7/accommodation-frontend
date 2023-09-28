f2ac09ff4683f7546ea11f72de7bed33beb024ae
nicolebass7 <nicole.bass@eagles.oc.edu>
2023-09-28 17:01:35 -0500

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ dns.setDefaultResultOrder("verbatim");
 
 export default () => {
   const baseURL =
-    process.env.APP_ENV === "development" ? "/" : "/tutorial-frontend-vue3/";
+    process.env.APP_ENV === "development" ? "/" : "/2023/projct3/t5";
 
   return defineConfig({
     plugins: [vue(), vuetify({ autoImport: true })],
