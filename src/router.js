6d9e81d86ba00826c2e1e435a9d94dbfcd7f8dc0
manda06 <amanda.bass@eagles.oc.edu>
2023-10-16 10:16:46 -0500

    created the stuent agreement page

--- src/router.js
+++ src/router.js
@@ -8,6 +8,7 @@ import AddTutorial from "./views/AddTutorial.vue";
 import ViewTutorial from "./views/ViewTutorial.vue";
 import AddLesson from "./views/AddLesson.vue";
 import EditLesson from "./views/EditLesson.vue";
+import Agreement from "./views/AgreementPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -51,6 +52,11 @@ const router = createRouter({
       name: "editLesson",
       component: EditLesson,
       props: true,
+    },
+    {
+      path: "/agreement",
+      name: "agreement",
+      component: Agreement,
     },
   ],
 });
---
+++ src/views/AgreementPage.vue
@@ -0,0 +1,49 @@
+<script setup>
+import TutorialServices from "../services/agreementServices";
+import Utils from "../config/utils.js";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const agreement = ref([]);
+const user = Utils.getStore("user");
+const message = ref("Do you agree for the information you are about to share can be shared with faculty and staff on campus to better help you?");
+
+const props = defineProps({
+  label: {
+      type: String,
+      default: ''
+    }
+});
+    
+</script>
+
+<template>
+  <div>
+    <v-container>
+      <v-toolbar>
+        <v-toolbar-title
+          >Hello, {{ user.fName }} {{ user.lName }}!</v-toolbar-title
+        >
+      </v-toolbar>
+      <br /><br />
+      <v-card>
+        <v-card-title> Agreement </v-card-title>
+        <v-card-text>
+          <b>{{ message }}</b>
+        </v-card-text>
+        <div>
+          <input
+          id="checkbox-1"
+          type="checkbox"
+          v-model="agreement"
+          class="field"
+          />
+          <label>I agree</label>
+          
+
+        </div>
+      </v-card>
+    </v-container>
+  </div>
+</template>
